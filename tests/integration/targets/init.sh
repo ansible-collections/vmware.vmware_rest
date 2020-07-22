92a82a0110e047f5ae0acb2bfec5b34dbe57ae9f
Gonéri Le Bouder <goneri@lebouder.net>
2020-07-22 16:53:02 -0400

    tests: only use the inventory file
    Only use the inventory file as source of trust. This will simplify the
    configure.

--- tests/integration/targets/init.sh
+++ tests/integration/targets/init.sh
@@ -9,19 +9,15 @@ INVENTORY_PATH="${INVENTORY_PATH:-../../inventory.networking}"
 # username=administrator@vsphere.local
 # password=TRQhZ:WdXrhA*w;nqaU0
 
-if [ -f ${INVENTORY_PATH} ]; then
-    echo "Reading credentials from inventory.networking"
-    export VMWARE_HOST=$(sed 's,^hostname=\(.*\),\1,;t;d' ${INVENTORY_PATH})
-    VMWARE_USER=$(sed 's,^username=\(.*\),\1,;t;d' ${INVENTORY_PATH})
-    VMWARE_PASSWORD=$(sed 's,^password=\(.*\),\1,;t;d' ${INVENTORY_PATH})
-elif [ -f /tmp/vcenter/tmp/vcenter_password.txt ]; then
-    echo "Reading credentials from /tmp/vcenter/tmp/vcenter_password.txt"
-    VMWARE_HOST="vcenter.test"
-    VMWARE_USER="administrator@vsphere.local"
-    VMWARE_PASSWORD="$(cat /tmp/vcenter/tmp/vcenter_password.txt)"
-fi
-
+echo "Reading credentials from ${INVENTORY_PATH}"
 export ANSIBLE_CONFIG=ansible.cfg
-export VMWARE_HOST
-export VMWARE_USER
-export VMWARE_PASSWORD
+export VMWARE_HOST=$(sed 's,^vcenter_hostname=\(.*\),\1,;t;d' ${INVENTORY_PATH})
+export VMWARE_USER=$(sed 's,^vcenter_username=\(.*\),\1,;t;d' ${INVENTORY_PATH})
+export VMWARE_PASSWORD=$(sed 's,^vcenter_password=\(.*\),\1,;t;d' ${INVENTORY_PATH})
+export ESXI1_HOSTNAME=$(sed 's,^esxi1_hostname=\(.*\),\1,;t;d' ${INVENTORY_PATH})
+export ESXI1_USERNAME=$(sed 's,^esxi1_username=\(.*\),\1,;t;d' ${INVENTORY_PATH})
+export ESXI1_PASSWORD=$(sed 's,^esxi1_password=\(.*\),\1,;t;d' ${INVENTORY_PATH})
+export ESXI2_HOSTNAME=$(sed 's,^esxi2_hostname=\(.*\),\1,;t;d' ${INVENTORY_PATH})
+export ESXI2_USERNAME=$(sed 's,^esxi2_username=\(.*\),\1,;t;d' ${INVENTORY_PATH})
+export ESXI2_PASSWORD=$(sed 's,^esxi2_password=\(.*\),\1,;t;d' ${INVENTORY_PATH})
+export VMWARE_VALIDATE_CERTS=no
