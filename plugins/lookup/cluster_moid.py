6da11e00e06231f403bcf51120a11f850f660b39
Gonéri Le Bouder <goneri@lebouder.net>
2021-11-29 11:30:10 -0500

    lookup: use the FQCN in examples
    Use the FQCN of the built-in modules.
    
    See: https://github.com/ansible-collections/ansible-inclusion/discussions/24

--- plugins/lookup/cluster_moid.py
+++ plugins/lookup/cluster_moid.py
@@ -27,17 +27,17 @@ extends_documentation_fragment:
 EXAMPLES = r"""
 # lookup sample
 - name: set connection info
-  set_fact:
+  ansible.builtin.set_fact:
     connection_args:
         vcenter_hostname: "vcenter.test"
         vcenter_username: "administrator@vsphere.local"
         vcenter_password: "1234"
 
 - name: lookup MoID of the object
-  debug: msg="{{ lookup('vmware.vmware_rest.cluster_moid', '/my_dc/host/my_cluster', **connection_args) }}"
+  ansible.builtin.debug: msg="{{ lookup('vmware.vmware_rest.cluster_moid', '/my_dc/host/my_cluster', **connection_args) }}"
 
 - name: lookup MoID of the object inside the path
-  debug: msg="{{ lookup('vmware.vmware_rest.cluster_moid', '/my_dc/host/') }}"
+  ansible.builtin.debug: msg="{{ lookup('vmware.vmware_rest.cluster_moid', '/my_dc/host/') }}"
 """
 
 
