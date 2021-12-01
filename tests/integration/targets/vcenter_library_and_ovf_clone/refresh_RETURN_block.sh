0f6e568cc34019a075bd0d00c904c504fa158078
Gonéri Le Bouder <goneri@lebouder.net>
2021-11-30 22:45:31 -0500

    tests: add vcenter_library_and_ovf_clone target (#284)
    tests: add vcenter_library_and_ovf_clone target
    
    Depends-On: #286
    Move all the library and OVF related tests in a new target.
    
    Reviewed-by: None <None>

---
+++ tests/integration/targets/vcenter_library_and_ovf_clone/refresh_RETURN_block.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2155
+set -eux
+
+export ANSIBLE_CALLBACK_WHITELIST=goneri.utils.collect_task_outputs
+export COLLECT_TASK_OUTPUTS_COLLECTION=vmware.vmware_rest
+export COLLECT_TASK_OUTPUTS_TARGET_DIR=$(realpath ../../../../manual/source/vmware_rest_scenarios/task_outputs/)
+export INVENTORY_PATH=/tmp/inventory-vmware_rest
+source ../init.sh
+exec ansible-playbook -i ${INVENTORY_PATH} playbook.yaml -e wait_for_vm=1
