f27025229f0f8bc7340c9636c5583a2d6d754fa7
Gonéri Le Bouder <goneri@lebouder.net>
2022-04-25 20:44:56 -0400

    vcenter_vm_scenario1: split playbook.yaml
    The two steps a in two different isolated playbooks for the
    Downstream-EE testing. The prepare.yaml depends on community.vmware
    which is not part of Downstream-EE.
    This allow use to run the first stage with a regular ansible, and
    the second part from within a Downstream-EE container.
    Please enter the commit message for your changes. Lines starting

--- tests/integration/targets/vcenter_vm_scenario1/runme.sh
+++ tests/integration/targets/vcenter_vm_scenario1/runme.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 source ../init.sh
-exec ansible-playbook playbook.yaml
+
+# NOTE, the two steps a in two different isolated playbooks for the
+# Downstream-EE testing. The prepare.yaml depends on community.vmware
+# which is not part of Downstream-EE.
+# This allow use to run the first stage with a regular ansible, and
+# the second part from within a Downstream-EE container.
+ansible-playbook prepare.yaml
+exec ansible-playbook run.yaml
