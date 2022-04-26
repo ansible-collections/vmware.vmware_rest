12a3c0be982eb211b5797e00fd7d3fc621a8b6ea
Gonéri Le Bouder <goneri@lebouder.net>
2022-04-26 17:16:43 -0400

    vcenter_vm_scenario1: split playbook.yaml (#318)
    vcenter_vm_scenario1: split playbook.yaml

--- tests/integration/targets/vcenter_vm_scenario1/refresh_RETURN_block.sh
+++ tests/integration/targets/vcenter_vm_scenario1/refresh_RETURN_block.sh
@@ -7,4 +7,5 @@ export COLLECT_TASK_OUTPUTS_COLLECTION=vmware.vmware_rest
 export COLLECT_TASK_OUTPUTS_TARGET_DIR=$(realpath ../../../../manual/source/vmware_rest_scenarios/task_outputs/)
 export INVENTORY_PATH=/tmp/inventory-vmware_rest
 source ../init.sh
-exec ansible-playbook -i ${INVENTORY_PATH} playbook.yaml -e wait_for_vm=1
+ansible-playbook prepare.yaml
+exec ansible-playbook run.yaml -e wait_for_vm=1
