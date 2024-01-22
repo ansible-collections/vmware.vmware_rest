90c67c681e8034aa2d22ae91eef6b937c82ec58a
Helen Bailey <hakbailey@gmail.com>
2024-01-22 08:50:17 -0500

    Fix options parsing to use vcenter_validate_certs false value (#462)
    Fix options parsing to use vcenter_validate_certs false value
    
    SUMMARY
    Fixes the credentials options parsing to use vcenter_validate_certs=false if provided. It was previously defaulting to the env variable for any falsy value of vcenter_validate_certs, which meant both None (option not provided) and False (passed by the user) values were ignored.
    Fixes #425
    ISSUE TYPE
    
    Bugfix Pull Request
    
    COMPONENT NAME
    plugin_utils.lookup
    
    Reviewed-by: Alina Buzachis
    Reviewed-by: Helen Bailey <hebailey@redhat.com>
    Reviewed-by: Mario Lenz <m@riolenz.de>
    Reviewed-by: Brian Coca
    Reviewed-by: Bikouo Aubin
    Reviewed-by: Mike Graves <mgraves@redhat.com>
    (cherry picked from commit f5d7fd256418e900016d4b606a33beb98fab552b)

--- plugins/plugin_utils/lookup.py
+++ plugins/plugin_utils/lookup.py
@@ -8,7 +8,6 @@ __metaclass__ = type
 
 
 import asyncio
-import os
 import urllib
 
 from ansible.errors import AnsibleLookupError
@@ -113,47 +112,24 @@ INVENTORY = {
 }
 
 
-def get_credentials(**options):
-    credentials = {}
-    credentials["vcenter_hostname"] = options.get("vcenter_hostname") or os.getenv(
-        "VMWARE_HOST"
-    )
-    credentials["vcenter_username"] = options.get("vcenter_username") or os.getenv(
-        "VMWARE_USER"
-    )
-    credentials["vcenter_password"] = options.get("vcenter_password") or os.getenv(
-        "VMWARE_PASSWORD"
-    )
-    credentials["vcenter_validate_certs"] = options.get(
-        "vcenter_validate_certs"
-    ) or os.getenv("VMWARE_VALIDATE_CERTS")
-    credentials["vcenter_rest_log_file"] = options.get(
-        "vcenter_rest_log_file"
-    ) or os.getenv("VMWARE_REST_LOG_FILE")
-    return credentials
-
-
 class Lookup:
     def __init__(self, options):
         self._options = options
 
     @classmethod
     async def entry_point(cls, terms, options):
+        if not terms or not terms[0]:
+            raise AnsibleLookupError(
+                "Option _terms is required but no object has been specified"
+            )
         session = None
-
-        if not options.get("vcenter_hostname"):
-            raise AnsibleLookupError("vcenter_hostname cannot be empty")
-        if not options.get("vcenter_username"):
-            raise AnsibleLookupError("vcenter_username cannot be empty")
-        if not options.get("vcenter_password"):
-            raise AnsibleLookupError("vcenter_password cannot be empty")
 
         try:
             session = await open_session(
-                vcenter_hostname=options.get("vcenter_hostname"),
-                vcenter_username=options.get("vcenter_username"),
-                vcenter_password=options.get("vcenter_password"),
-                validate_certs=bool(options.get("vcenter_validate_certs")),
+                vcenter_hostname=options["vcenter_hostname"],
+                vcenter_username=options["vcenter_username"],
+                vcenter_password=options["vcenter_password"],
+                validate_certs=options.get("vcenter_validate_certs"),
                 log_file=options.get("vcenter_rest_log_file"),
             )
         except EmbeddedModuleFailure as e:
@@ -163,9 +139,6 @@ class Lookup:
 
         lookup = cls(options)
         lookup._options["session"] = session
-
-        if not terms:
-            raise AnsibleLookupError("No object has been specified.")
 
         task = asyncio.ensure_future(lookup.moid(terms[0]))
 
