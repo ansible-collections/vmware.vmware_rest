618a19dd44614685db3e5671173b410e2a5134e5
mikemorency <mikemorency93@gmail.com>
2024-08-09 09:57:57 -0400

    updating lookup documentation and examples (#503)
    SUMMARY
    The lookup plugin documentation needs to be updated. Its missing useful examples, inconsistent, and can be vague. These changes should address those issues.
    ISSUE TYPE
    
    Docs Pull Request
    
    COMPONENT NAME
    all lookup plugins (plugins/lookup/*)
    
    Reviewed-by: Brian Coca
    Reviewed-by: mikemorency
    Reviewed-by: Alina Buzachis

--- plugins/lookup/vm_moid.py
+++ plugins/lookup/vm_moid.py
@@ -11,33 +11,99 @@ DOCUMENTATION = r"""
 name: vm_moid
 short_description: Look up MoID for vSphere vm objects using vCenter REST API
 description:
-    - Returns Managed Object Reference (MoID) of the vSphere vm object contained in the specified path.
+  - Returns Managed Object Reference (MoID) of the vSphere vm object contained in the specified path.
 author:
-    - Alina Buzachis (@alinabuzachis)
+  - Alina Buzachis (@alinabuzachis)
 version_added: 2.1.0
 requirements:
-    - vSphere 7.0.3 or greater
-    - python >= 3.6
-    - aiohttp
+  - vSphere 7.0.3 or greater
+  - python >= 3.6
+  - aiohttp
 extends_documentation_fragment:
-- vmware.vmware_rest.moid
+  - vmware.vmware_rest.moid
 """
 
 
 EXAMPLES = r"""
-# lookup sample
-- name: set connection info
-  ansible.builtin.set_fact:
+#
+#
+# The examples below assume you have a datacenter named 'my_dc' and a vm named 'my_vm'.
+# Replace these values as needed for your environment.
+#
+#
+
+#
+# Authentication / Connection Arguments
+#
+# You can explicitly set the connection arguments in each lookup. This may be clearer for some use cases
+- name: Pass In Connection Arguments Explicitly
+  ansible.builtin.debug:
+    msg: >-
+      {{ lookup('vmware.vmware_rest.vm_moid', '/my_dc/vm/my_cluster/my_vm',
+      vcenter_hostname="vcenter.test",
+      vcenter_username="administrator@vsphere.local",
+      vcenter_password="1234") }}
+
+# Alternatively, you can add the connection arguments to a dictionary variable, and then pass that variable to the
+# lookup plugins. This makes the individual lookup plugin calls simpler
+- name: Example Playbook
+  hosts: all
+  vars:
     connection_args:
-        vcenter_hostname: "vcenter.test"
-        vcenter_username: "administrator@vsphere.local"
-        vcenter_password: "1234"
+      vcenter_hostname: "vcenter.test"
+      vcenter_username: "administrator@vsphere.local"
+      vcenter_password: "1234"
+  tasks:
+    # Add more tasks or lookups as needed, referencing the same connection_args variable
+    - name: Lookup MoID of the object
+      ansible.builtin.debug:
+        msg: "{{ lookup('vmware.vmware_rest.vm_moid', '/my_dc/vm/my_cluster/my_vm', **connection_args) }}"
 
-- name: lookup MoID of the object
-  ansible.builtin.debug: msg="{{ lookup('vmware.vmware_rest.vm_moid', '/my_dc/host/my_cluster/esxi1.test/test_vm1', **connection_args) }}"
+# Finally, you can also leverage the environment variables associated with each connection arg, and avoid passing
+# extra args to the lookup plugins
+- name: Use a lookup plugin with VMWARE_* environment variables set
+  ansible.builtin.debug:
+    msg: "{{ lookup('vmware.vmware_rest.vm_moid', '/my_dc/vm/my_cluster/my_vm') }}"
 
-- name: lookup MoID of the object inside the path
-  ansible.builtin.debug: msg="{{ lookup('vmware.vmware_rest.vm_moid', '/my_dc/vm/') }}"
+#
+# VM Search Path Examples
+#
+# VMs are located under the 'vm' folder in a datacenter. If they are not in a folder, the path
+# should include the cluster name.
+# The basic path for a VM should look like '/<datacenter-name>/vm/<cluster-name>/<vm-name>'
+- name: Lookup VM Named 'my_vm' in Datacenter 'my_dc' in Cluster 'my_cluster'
+  ansible.builtin.debug:
+    msg: "{{ lookup('vmware.vmware_rest.folder_moid', '/my_dc/vm/my_cluster/my_vm') }}"
+
+# If the VM is in a user created VM folder, you just include the folder name.
+- name: Lookup VM Named 'my_vm' in Datacenter 'my_dc' in Folder 'production' (also in Cluster 'my_cluster')
+  ansible.builtin.debug:
+    msg: "{{ lookup('vmware.vmware_rest.folder_moid', '/my_dc/production/my_vm') }}"
+
+#
+# Usage in Playbooks
+#
+#
+# The lookup plugin can be used to simplify your playbook. Here is an example of how you might use it.
+#
+# Without the lookup, this takes two modules which both run on the remote host. This can slow down execution
+# and adds extra steps to the playbook:
+- name: Look up the VM called 'my_vm' in the inventory
+  vmware.vmware_rest.vcenter_vm_info:
+    filter_names:
+      - my_vm
+  register: my_vm_info
+
+- name: Delete a VM
+  vmware.vmware_rest.vcenter_vm:
+    vm: '{{ my_vm_info.value[0].vm }}'
+    state: absent
+
+# With the lookup, playbooks are shorter, quicker, and more intuitive:
+- name: Delete a VM
+  vmware.vmware_rest.vcenter_vm:
+    vm: "{{ lookup('vmware.vmware_rest.vm_moid', '/my_dc/vm/my_vm') }}"
+    state: absent
 """
 
 
