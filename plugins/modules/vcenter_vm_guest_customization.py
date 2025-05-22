1b2064a0b09880f382efabbdd1b4936d59ba3d2a
Mike Morency <mikemorency93@gmail.com>
2025-05-22 10:26:21 -0400

    fix sanity failures

--- plugins/modules/vcenter_vm_guest_customization.py
+++ plugins/modules/vcenter_vm_guest_customization.py
@@ -39,7 +39,8 @@ options:
             system.
         - '     - sysprep_xml (string): All settings specified in a XML format. This
             is the content of a typical answer.xml file that is used by System administrators
-            during the Windows image customization. Check https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/update-windows-settings-and-scripts-create-your-own-answer-file-sxs
+            during the Windows image customization. Check
+            https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/update-windows-settings-and-scripts-create-your-own-answer-file-sxs
             Exactly one of WindowsConfiguration.sysprep or WindowsConfiguration.sysprep-xml
             must be specified.'
         - If unset, sysprep settings will not be applied to the windows guest operating
