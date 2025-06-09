ea44592eb6f2952a5ed22596cb00aad301bdeb82
Mike Morency <mikemorency93@gmail.com>
2025-06-09 10:23:04 -0400

    deprecate lookup plugins

--- plugins/doc_fragments/moid.py
+++ plugins/doc_fragments/moid.py
@@ -18,6 +18,14 @@ class ModuleDocFragment(object):
             use them, instead of requiring multiple modules and tasks to do the same thing.
             See the examples section for a comparison.
 
+    deprecated:
+        removed_in: 5.0.0
+        why: >-
+          This plugin will not work with ansible-core 2.19 and above. Refer to
+          L(the migration documentation,https://github.com/ansible-collections/vmware.vmware/tree/main/docs/lookup_plugin_migration.md)
+          for migration examples.
+        alternative: Use M(vmware.vmware.moid_from_path) instead.
+
     options:
         _terms:
             description:
