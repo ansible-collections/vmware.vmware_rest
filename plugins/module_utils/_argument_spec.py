ed8f0ac5eef22eb561f004374d043c4900654f1d
Mike Morency <mikemorency93@gmail.com>
2026-06-17 21:15:11 -0400

    sanity

--- plugins/module_utils/_argument_spec.py
+++ plugins/module_utils/_argument_spec.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2026, Eco Ansible Content Team <@eco-ansible-content>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
 from ansible.module_utils.basic import env_fallback
 
 
