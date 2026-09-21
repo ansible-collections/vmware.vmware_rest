3be59eb3bf8a0166a3f512669f50e286efa15565
Mike Morency <mikemorency93@gmail.com>
2026-09-21 17:20:24 -0400

    add missing header

--- plugins/module_utils/_operation_configs.py
+++ plugins/module_utils/_operation_configs.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2026, Eco Ansible Content Team <@eco-ansible-content>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
 from typing import Union
 import re
 from ._errors import (
